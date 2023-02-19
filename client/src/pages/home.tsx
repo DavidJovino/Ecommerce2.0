9c7bb5a3be58bf829d9fab7bcfb714ac6a001736
David Jovino <davidjovino@live.com>
2023-02-18 22:04:22 -0300

    Edit Property and minor updates

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -10,6 +10,20 @@ import {
 } from 'components';
 
 const Home = () => {
+  const { data, isLoading, isError } = useList({
+    resource: 'properties',
+    config: {
+      pagination: {
+        pageSize: 4,
+      },
+    },
+  });
+
+  const latestProperties = data?.data ?? [];
+
+  if (isLoading) return <Typography>Loading...</Typography>;
+  if (isError) return <Typography>Something went wrong!</Typography>;
+
   return (
     <Box>
       <Typography fontSize={25} fontWeight={700} color="#11142D">
@@ -66,6 +80,18 @@ const Home = () => {
         <Typography fontSize="18px" fontWeight={600} color="#11142d">
           Latest Properties
         </Typography>
+        <Box mt={2.5} sx={{ display: 'flex', flexWrap: 'wrap', gap: 4 }}>
+          {latestProperties.map((property) => (
+            <PropertyCard
+              key={property._id}
+              id={property._id}
+              title={property.title}
+              location={property.location}
+              price={property.price}
+              photo={property.photo}
+            />
+          ))}
+        </Box>
       </Box>
     </Box>
   );
